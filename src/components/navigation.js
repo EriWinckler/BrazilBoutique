c852c7b853b72d38f08c5bffee9537cef2a562b3
Eridan <efwinckler@gmail.com>
2020-10-10 13:22:48 -0400

    fixed navbar links

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 /**
 * @author
@@ -12,9 +13,9 @@ const Navigation = (props) => {
     <Navbar bg="primary" variant="dark" sticky="top">
       <Navbar.Brand href="/">Brazil Boutique</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="/" exact>Home</Nav.Link>
-          <Nav.Link href="/tops">Tops</Nav.Link>
-          <Nav.Link href="/pants">Pants</Nav.Link>
+        <Nav.Link as={Link} to="/" exact>Home</Nav.Link>
+        <Nav.Link as={Link} to="/tops">Tops</Nav.Link>
+        <Nav.Link as={Link} to="/pants">Pants</Nav.Link>
         </Nav>
         <Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
