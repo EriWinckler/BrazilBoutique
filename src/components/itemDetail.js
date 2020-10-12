3582c316834c20fc59e91cdb0e029fd22e735212
Eridan <efwinckler@gmail.com>
2020-10-11 22:20:27 -0400

    added styling for the itemDetail

--- src/components/itemDetail.js
+++ src/components/itemDetail.js
@@ -1,34 +1,52 @@
-import React, { useState, useEffect } from "react";
-import data from '../data/data';
+import React from "react";
+import { AwesomeButton } from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
+import data from "../data/data";
 
 /**
  * @author
  * @function ItemDetail
  **/
 
-const ItemDetail = ({match}) => {
-  
 
+
+const ItemDetail = ({ match }) => {
   return (
     <section id="itemDetail">
       <div className="store">
         {data.map((data, i) => {
           return data.id === `${match.params.id}` ? (
-            <div key={data.title}>
-              <div className="container">
-                <img
-                  src={data.image}
-                  alt={data.imageAlt}
-                  className="productImg"
-                />
+            <div>
+              <div key={data.title}>
+                <div className="container">
+                  <img
+                    src={data.image}
+                    alt={data.imageAlt}
+                    className="productImg"
+                  />
+                </div>
+              </div>
+              <div className="description">
                 <h5>{data.title}</h5>
-
                 <p>{data.subtitle ? data.subtitle : null}</p>
                 <p>{data.price ? `$ ${data.price}` : null}</p>
+                <p className="details">
+                  {data.details ? `${data.details}` : null}
+                </p>
               </div>
             </div>
           ) : null;
         })}
+      </div>
+      <div className="description">
+        <AwesomeButton 
+          type="primary" 
+          size="medium"
+          
+        >
+          
+        Add to Cart
+        </AwesomeButton>
       </div>
     </section>
   );
--- src/data/data.js
+++ src/data/data.js
@@ -2,107 +2,107 @@ export default [
     {
         id: "1",
         category: "Tops",
-        title: "White Top",
+        title: "Lorem Ipsum",
         subtitle: "Win the views!",
         image: "https://picsum.photos/300/350",
-        imageAlt: "White T-shirt",
+        imageAlt: "Lorem Ipsum",
+        details: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla malesuada pellentesque elit eget gravida. Orci dapibus ultrices in iaculis nunc sed augue lacus.",
         price: 19.99
     },
     {
         id: "2",
         category: "Tops",
-        title: "Red Top",
+        title: "Venenatis a Condimentum",
         subtitle: "Win the views!",
         image: "https://picsum.photos/250/400",
-        imageAlt: "Red T-shirt"
+        imageAlt: "Venenatis a Condimentum"
     },
     {
         id: "3",
-        category: "Tops",
+        category: "Tortor at Auctor",
         title: "Red Top",
         image: "https://picsum.photos/300/600",
-        imageAlt: "Red T-shirt"
+        imageAlt: "Tortor at Auctor"
     },
     {
         id: "4",
         category: "Tops",
-        title: "Red Top",
+        title: "Eget Nunc Lobortis",
         subtitle: "Win the views!",
         image: "https://picsum.photos/125/400",
-        imageAlt: "Red T-shirt",
-        link: 'https://www.google.com',
+        imageAlt: "Eget Nunc Lobortis",
         price: 28.99
     },
     {
         id: "5",
         category: "Tops",
-        title: "Red Top",
+        title: "Quam Adipiscing Vitae",
         subtitle: "Win the views!",
         image: "https://picsum.photos/300/400",
-        imageAlt: "Red T-shirt"
+        imageAlt: "Quam Adipiscing Vitae"
     },
     {
         id: "6",
         category: "Tops",
-        title: "Red Top",
+        title: "Arcu Cursus",
         subtitle: "Win the views!",
         image: "https://picsum.photos/300/400",
-        imageAlt: "Red T-shirt"
+        imageAlt: "Arcu Cursus"
     },
     {
         id: "7",
-        category: "Tops",
+        category: "Est Ultricies",
         title: "Red Top",
         subtitle: "Win the views!",
         image: "https://picsum.photos/300/400",
-        imageAlt: "Red T-shirt"
+        imageAlt: "Est Ultricies"
     },
     {
         id: "8",
         category: "Pants",
-        title: "White Pants",
+        title: "Egestas Erat",
         subtitle: "Win the views again!",
         image: "https://picsum.photos/300/400",
-        imageAlt: "White Pants",
+        imageAlt: "Egestas Erat",
         link: 'https://www.google.com',
         price: 19.99
     },
     {
         id: "9",
         category: "Pants",
-        title: "White Pants",
+        title: "Netus Et Malesuada Fames",
         subtitle: "Win the views again!",
         image: "https://picsum.photos/250/350",
-        imageAlt: "White Pants",
+        imageAlt: "Netus Et Malesuada Fames",
         link: 'https://www.google.com'
     },
     {
         id: "10",
         category: "Pants",
-        title: "White Pants",
+        title: "Ultrices",
         subtitle: "Win the views again!",
         image: "https://picsum.photos/200/300",
-        imageAlt: "White Pants",
+        imageAlt: "Ultrices",
         link: 'https://www.google.com',
         price: 19.99
     },
     {
         id: "11",
         category: "Pants",
-        title: "White Pants",
+        title: "Placerat",
         subtitle: "Win the views again!",
         image: "https://picsum.photos/300/350",
-        imageAlt: "White Pants",
+        imageAlt: "Placerat",
         link: 'https://www.google.com',
         price: 19.99
     },
     {
         id: "12",
         category: "Pants",
-        title: "White Pants",
+        title: "Amet",
         subtitle: "Win the views again!",
         image: "https://picsum.photos/300/350",
-        imageAlt: "White Pants",
+        imageAlt: "Amet",
         link: 'https://www.google.com',
         price: 19.99
     },
